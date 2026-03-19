fcd3f894e0bc5e35cfa51b2e1347e1f2bcd23242
P V ADHITHYA <121473249+Mysterious-27@users.noreply.github.com>
2026-03-19 23:06:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import CharacterTextSplitter
+from langchain_text_splitters import CharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 # from langchain_community.llms import Ollama
